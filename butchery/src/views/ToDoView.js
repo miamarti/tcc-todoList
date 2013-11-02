7b06ff0d8d06e4e206480eb5dabed8bf570e4f64
miamarti <miller.augusto@gmail.com>
2013-11-02 13:41:02 -0200

    Publicando

--- butchery/src/views/ToDoView.js
+++ butchery/src/views/ToDoView.js
@@ -1,15 +1,7 @@
 define(function(){
 	var _this = {
-		getTable: function(){
-			return '<table class="table table-hover"><thead><tr><th>Event</th><th>Urgent</th><th>Planned To</th><th>Canceled</th></tr></thead><tbody></tbody></table>';
-		},
-		
-		getLineTable: function(toDoBean){
-			return '<tr><td>' + toDoBean.getTitle() + '</td><td>' + toDoBean.getUrgent() + '</td><td>' + toDoBean.getPlannedTo() + '</td><td>' + toDoBean.getEnabled() + '</td></tr>';
-		},
-		
 		getLineList: function(toDoBean){
-			return '<div><span class="fc-titleList">' + toDoBean.getTitle() + '</span><span class="fc-urgent">' + toDoBean.getUrgent() + '</span></div>';
+			return '<div><span class="fc-titleList">' + toDoBean.getEnabled()[0] + ' ' + toDoBean.getTitle() + ' ' + toDoBean.getEnabled()[1] + '</span><span class="fc-icons">' + toDoBean.getUrgent() + '</span></div>';
 		}
 	};
 	return _this;
